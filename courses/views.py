0f8fcf343473ad68527689a63fa0fd799faeabb2
patidarparas13 <patidarparas13@gmail.com>
2018-10-30 18:28:04 +0530

    creating a class based views and authentication templates

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from .models import Course
 # Create your views here.
+
+class ManageCoursesListView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super(ManageCoursesListView,self).get_queryset()
+        return qs.filter(owner = self.request.user)
